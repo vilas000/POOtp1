6e7d697536663a7d5e3e46f2d79a55ff62644c29
yasminaa18 <yasminaltino8@gmail.com>
2024-08-15 22:29:40 -0300

    Commit final

--- menu.cpp
+++ menu.cpp
@@ -9,13 +9,11 @@ Menu::Menu() {
     users.push_back(new Vendedor(03, "vendedor3", "0003", "Marcelo"));
     users.push_back(new Vendedor(04, "vendedor4", "0004", "Nicole"));
     users.push_back(new Vendedor(05, "vendedor5", "0005", "Carlos"));
-    users.push_back(new Vendedor(00, "v", "1", "Manuel Debug"));
     users.push_back(new Mecanico("mecanico1", "Hugo", "0011", 11));
     users.push_back(new Mecanico("mecanico2", "Paula", "0012", 12));
     users.push_back(new Mecanico("mecanico3", "Jadson", "0013", 13));
     users.push_back(new Mecanico("mecanico4", "Marina", "0014", 14));
     users.push_back(new Mecanico("mecanico5", "Luis", "0015", 15));
-    users.push_back(new Mecanico("m", "Manuelito Debug", "1", 10));
 
     clientes.push_back({"Roberto", "3434", 143, "fox", "AB88"});
     clientes.push_back({"Sabrina", "2030", 2856, "celta", "NK20"});
